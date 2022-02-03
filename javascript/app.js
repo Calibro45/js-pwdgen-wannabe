ba47272aa9bc63d07164682b40ead00dc4ed3356
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-03 16:00:45 +0100

    add csual number

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,32 @@
 /* alert("hello world")
 console.log(alert) */
 
+//nome
+
 const userName = prompt("Come ti chiami?", "Ajeje");
 console.log(userName)
+
+//cognome
 
 const userSurname = prompt("Qul'è il tup cognome?", "Brazorf");
 console.log(userSurname)
 
+//colore
+
 const userColor = prompt("Qual'è il tuo colore preferito?", "Puffo_Torroncino");
 console.log(userColor)
+
+//inizializzazione variabile contenuto password
 
 const PASSWORD = document.getElementById("password");
 console.log(PASSWORD)
 
-PASSWORD.innerHTML = ` ${userName}${userSurname}${userColor}`
+//numero
+
+const NUMBER = Math.floor(Math.random() * 100)
+console.log(NUMBER)
+
+//mostra password
+
+PASSWORD.innerHTML = ` ${userName}${userSurname}${userColor}${NUMBER}`
 console.log(PASSWORD.innerHTML)
