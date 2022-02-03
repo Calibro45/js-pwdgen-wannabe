28ea077ef78c0942a60b4abb2e2a559d34f06fe2
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-03 15:10:55 +0100

    chiedi nome

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+/* alert("hello world")
+console.log(alert) */
+
+const userName = prompt("come ti chiami?", "Ajeje");
+console.log(userName)
+
+const userSurname = prompt("qul'è il tup cognome?", "Brazorf");
