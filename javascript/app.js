442ec3639447fbc169d02d767880005db2b1d285
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-03 15:17:31 +0100

    add color

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,11 @@
 /* alert("hello world")
 console.log(alert) */
 
-const userName = prompt("come ti chiami?", "Ajeje");
+const userName = prompt("Come ti chiami?", "Ajeje");
 console.log(userName)
 
-const userSurname = prompt("qul'è il tup cognome?", "Brazorf");
+const userSurname = prompt("Qul'è il tup cognome?", "Brazorf");
 console.log(userSurname)
+
+const userColor = prompt("Qual'è il tuo colore preferito?", "Puffo e Torroncino");
+console.log(userColor)
